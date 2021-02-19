805d2147d5dfbc9b2349d1283a9054e99f4263b7
Aaron Votre <avotre422@gmail.com>
2021-02-19 08:15:26 -0500

    ✅ Add CodeVis & Repo Tests

--- test/code_vis_test.exs
+++ test/code_vis_test.exs
@@ -1,6 +1,6 @@
 defmodule CodeVisTest do
   use ExUnit.Case
-  alias CodeVis.ProjectAnalysis
+  alias CodeVis.{ProjectAnalysis, Repo}
 
   describe "ProjectAnalysis" do
     test "user_modules/0" do
@@ -54,6 +54,49 @@ defmodule CodeVisTest do
       assert result =~ "v1 -> v2"
       assert result =~ "v0 -> v2"
       refute result =~ "v2 -> v1"
+    end
+  end
+
+  describe "CodeVis" do
+    test "can map recursive calls" do
+      Repo.start()
+      mfa_main = {Main, :fxn, 0}
+      mfa_1 = {Main.One, :fxn, 0}
+      mfa_2 = {Main.Two, :fxn, 0}
+      user_modules = [Main, Main.One, Main.Two]
+
+      Repo.insert({mfa_main, mfa_1})
+      Repo.insert({mfa_main, mfa_2})
+      _recursive_call = Repo.insert({mfa_2, mfa_main})
+
+      adj_map = CodeVis.build_adjacency_map(%{}, mfa_main, user_modules)
+
+      assert Map.has_key?(adj_map, mfa_main)
+      assert Map.has_key?(adj_map, mfa_1)
+      assert Map.has_key?(adj_map, mfa_2)
+      assert length(Map.keys(adj_map)) == 3
+    end
+
+    test "ignores non-user modules" do
+      Repo.start()
+      mfa_main = {Main, :fxn, 0}
+      mfa_1 = {Dependency.Main, :fxn, 0}
+      mfa_2 = {Elixir.BuiltIn, :fxn, 0}
+      user_modules = [Main]
+
+      Repo.insert({mfa_main, mfa_1})
+      Repo.insert({mfa_main, mfa_2})
+
+      adj_map = CodeVis.build_adjacency_map(%{}, mfa_main, user_modules)
+
+      assert Map.has_key?(adj_map, mfa_main)
+      assert Map.get(adj_map, mfa_main) == %{children: []}
+    end
+  end
+
+  describe "Repo" do
+    test "rejects invalid input" do
+      assert_raise ArgumentError, fn -> Repo.insert({"not a tuple!", {Main, :fxn, 0}}) end
     end
   end
 
